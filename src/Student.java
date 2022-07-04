03abefa791b60c323b810042580428c785962215
athmar mubark <am222xc@student.lnu.se>
2022-07-04 11:37:10 +0200

    Just new class test

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Student {
+
+    public static void main (String[] args) {
+        Test3 studen = new Test3("Athmar", "mubark");
+        System.out.println("New Student :  " + studen);
+    }
+
+}
