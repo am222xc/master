48c417640e3b1110c69f6830eee95a34fd512418
athmar mubark <am222xc@student.lnu.se>
2022-07-05 10:48:07 +0200

    Fixing errors

--- src/Demo2.java
+++ src/Demo2.java
@@ -4,8 +4,8 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test2");
-     price pr = new price(50.0, 3,50);
-     var prices = List.of(new price(50.0, 3,50),new price(90, 5,20));
+     price pr = new price(50.0, 3);
+     var prices = List.of(new price(50.0, 3),new price(90, 5));
      prices.forEach(System.out::println);
     }
 }
