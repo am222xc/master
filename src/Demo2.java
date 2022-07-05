ad02377b278b8da5c5b44382b680fbabff7717e6
athmar mubark <am222xc@student.lnu.se>
2022-07-05 10:35:59 +0200

    last changes will be added to feature branch

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,7 +1,11 @@
+import java.util.List;
+
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test2");
-
+     price pr = new price(50.0, 3,50);
+     var prices = List.of(new price(50.0, 3,50),new price(90, 5,20));
+     prices.forEach(System.out::println);
     }
 }
